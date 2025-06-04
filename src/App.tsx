2a1bce13c82ab293d3f958f5a302f9c7383fc618
edeparcevaux <edeparcevaux@reeliant.com>
2025-06-05 00:01:56 +0200

    [FEAT] Ajout du nombre de caisse de vin achetable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Cart, Explore, Home, Preview } from "./pages/index";
-import Navbar from "./components/Navbar.tsx";
+import "./styles/styles.scss";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 
 import {data} from "../assets/data";
-import BottleCard from "../components/BottleCard";
+import WineCard from "../components/WineCard";
 import {BottleResponseDto} from "../state/bottle/dto/BottleResponseDto";
 
 const Explore = () => {
@@ -19,7 +19,7 @@ const Explore = () => {
     <div className="">
       <div className="w-full min-h-fit p-10 md:p-20 grid grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4 xl:gap-10mx-auto ">
         {items.map((bottle, idx) => (
-          <BottleCard key={bottle.id} bottle={bottle}  setCard={setCart}/>
+          <WineCard key={bottle.id} bottle={bottle} setCard={setCart}/>
         ))}
       </div>
     </div>
